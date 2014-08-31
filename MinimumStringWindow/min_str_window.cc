1b8fd3c98e8ae2328d69e104625dd53b33312e80
Alex <yinchunxiang@gmail.com>
2014-08-31 13:42:22 +0800

    add mininum

--- MinimumStringWindow/min_str_window.cc
+++ MinimumStringWindow/min_str_window.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <tr1/unordered_map> 
+#include <string>
 
 using namespace std;
 using namespace tr1;
@@ -11,30 +12,40 @@ string minWindow(string S, string T) {
     string ret;
     int min_len = S.size() + 1;
     int unique_match = 0;
-    int window_size = 0; 
+    int window_size = 0;
     unordered_map<char, int> char_map;
     for (int i = 0; i < T.size(); ++i) {
         char_map[T[i]] = 0;
     }
     int start = 0;;
     for (int i = 0; i < S.size(); ++i) {
-        if (!char_map(S[i])) {
+        unordered_map<char, int>::iterator it = char_map.find(S[i]);
+        if (it == char_map.end()) {
+            continue;
+        }
+        if (0 == it->second ++) {
             unique_match ++;
         }
-        char_map[S[i]] ++;
         if (unique_match >= T.size()) {
             for (; start <= i ; ++ start){
-                if (char_map[S[start]] > 1){
-                    char_map[S[start]] --;
+		unordered_map<char, int>::iterator it = char_map.find(S[start]);
+		if (char_map.end() == it){
+		    continue;
+		}
+                if (it->second <= 1){
+                    break;
                 }
                 else {
-                    break;
+                    it->second --;
                 }
             }
             int curr_window_size = i - start + 1;
             if (curr_window_size < min_len){
                 min_len = curr_window_size;
-                ret = string(start, curr_window_size);
+                ret = string(S, start, curr_window_size);
+                cout << "start: " << start << endl;
+                cout << "i: " << i << endl;
+                cout << "ret: " << endl;
             }
         }
     }
@@ -43,8 +54,8 @@ string minWindow(string S, string T) {
 
 int main(int argc, const char *argv[])
 {
-    S = "ADOBECODEBANC";
-    T = "ABC";
+    string S = "ADOBECODEBANC";
+    string T = "ABC";
     cout << minWindow(S, T) << endl;
     return 0;
 }
