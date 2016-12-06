d26d668c862bc9c1992f04e44ec32b99272b996f
alexkkk <inc1ude@outlook.com>
2016-12-06 21:32:58 +0800

    update

--- Daily/20161206.LeftViewTree/main.cc
+++ Daily/20161206.LeftViewTree/main.cc
@@ -15,13 +15,25 @@
  **/ 
  
 struct Node {
-    int data;
-    Node *left;
-    Node *right
+    Node(int data): _data(data){
+    }
+    int data_;
+    Node *left_ = NULL;
+    Node *right_ = NULL;
+}
+
+void LeftView(Node* root) {
+
 }
 
 
-
+int main(int argc, char** argv) {
+    Node *root = new Node(12);
+    root->left_ = new Node(10);
+    root->right_ = new Node(30);
+    root->right_->left_ = new Node(25);
+    root->right_->right = new Node(40);
+}
 
 
 
