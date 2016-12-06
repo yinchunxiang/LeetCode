4cb3f2eec2bb8ed53c5d9417607187bacf32d9a0
alexkkk <inc1ude@outlook.com>
2016-12-06 21:09:13 +0800

    add Daily

---
+++ Daily/20161206.LeftViewTree/main.cc
@@ -0,0 +1,34 @@
+/****************************************************************************
+ * 
+ * Copyright (C) 2016 @com, Inc. All rights reserved.
+ * 
+ ****************************************************************************/
+
+
+
+/** 
+ * @file main.cc
+ * @author yinchunxiang(@com)
+ * @date 2016/12/06 20:59:07
+ * @brief 
+ * 
+ **/ 
+ 
+struct Node {
+    int data;
+    Node *left;
+    Node *right
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
---
+++ Daily/Include/tree.cc
@@ -0,0 +1,47 @@
+/****************************************************************************
+ * 
+ * Copyright (C) 2016 @com, Inc. All rights reserved.
+ * 
+ ****************************************************************************/
+
+
+
+/** 
+ * @file tree.cc
+ * @author yinchunxiang(@com)
+ * @date 2016/12/06 21:01:51
+ * @brief 
+ * 
+ **/ 
+ 
+class Tree {
+    struct Node {
+        int data;
+        Node* left;
+        Node* right;
+    };
+
+public:
+    /// 根据数组创建树
+    Tree(const vector<int>& v) {
+
+    }
+
+private:
+    Node* root_;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
