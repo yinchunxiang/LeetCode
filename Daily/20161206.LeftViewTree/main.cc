2b0b353a7b374cd10b4d1b98eb0f6ec570372d47
alexkkk <inc1ude@outlook.com>
2016-12-07 17:59:48 +0800

    update left view

--- Daily/20161206.LeftViewTree/main.cc
+++ Daily/20161206.LeftViewTree/main.cc
@@ -13,17 +13,37 @@
  * @brief 
  * 
  **/ 
+
+#include <list>
  
 struct Node {
-    Node(int data): _data(data){
+    Node(int data): data_(data){
     }
     int data_;
-    Node *left_ = NULL;
-    Node *right_ = NULL;
-}
+    Node *left_ = nullptr;
+    Node *right_ = nullptr;
+};
 
 void LeftView(Node* root) {
-
+    if (NULL == root) {
+        return;
+    }
+    std::list<Node*> cur_level;
+    std::list<Node*> next_level;
+    cur_level.push_back(root);
+    while (!cur_level.empty()) {
+        printf("%d\n", cur_level.front()->data_);
+        for(auto&& node : cur_level) {
+            if (node->left_) {
+                next_level.push_back(node->left_);
+            }
+            if (node->right_) {
+                next_level.push_back(node->right_);
+            }
+        }
+        cur_level.clear();
+        cur_level.swap(next_level);
+    }
 }
 
 
@@ -32,7 +52,8 @@ int main(int argc, char** argv) {
     root->left_ = new Node(10);
     root->right_ = new Node(30);
     root->right_->left_ = new Node(25);
-    root->right_->right = new Node(40);
+    root->right_->right_ = new Node(40);
+    LeftView(root);
 }
 
 
