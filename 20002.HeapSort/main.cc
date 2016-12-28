48bd9e433c4eec96abf30d78085a9390d41fbfcc
yinchunxiang <inc1ude@outlook.com>
2016-12-29 00:53:00 +0800

    implement heap sort

--- 20002.HeapSort/main.cc
+++ 20002.HeapSort/main.cc
@@ -1,4 +1,5 @@
 /****************************************************************************
+ *
  * 
  * Copyright (C) 2016 @com, Inc. All rights reserved.
  * 
@@ -15,13 +16,64 @@
  **/ 
 
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
-void printv(const vector<int>& v) {
-    return;
+using namespace std;
+
+
+void printv(vector<int>& v) {
+    for (int i = 0; i < v.size(); ++i) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+void rerange(vector<int>&v, int index, int maxn) {
+    while (index < maxn) {
+        auto lc = index * 2 + 1;
+        auto rc = index * 2 + 2;
+        auto largest = index;
+        if (lc < maxn && v[lc] > v[index]) {
+            swap(v[lc], v[index]);
+            largest = lc;
+        }
+        if (rc < maxn && v[rc] > v[index]) {
+            swap(v[rc], v[index]);
+            largest = rc;
+        }
+        if (index == largest) {
+            break;
+        } else {
+            index = largest;
+        }
+    }
+}
+
+void extract(vector<int> &v) {
+    auto n = v.size();
+    if (n <=0) return;
+
+    for (auto i = n - 1; i > 0; --i) {
+        swap(v[0], v[i]);
+        rerange(v, 0, i);
+    }
+}
+
+void HeapSort(vector<int> &v) {
+    auto n = int(v.size());
+    if (n <= 0) return;
+    int index = (n - 1)/2 -1;
+    for (; index >= 0; --index) {
+
+        rerange(v, index, n);
+    }
+    extract(v);
 }
 
 int main() {
     vector<int> v = {12, 11, 13, 5, 6, 7};
+    HeapSort(v);
     printv(v);
 }
 
