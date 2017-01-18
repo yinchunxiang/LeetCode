4b9f08a221629a4730b343c3dbe5f17912259091
yinchunxiang <yinchunxiang@gmail.com>
2017-01-18 17:42:52 +0800

    fix stl method bug

--- 20001.MergeSort/main.cc
+++ 20001.MergeSort/main.cc
@@ -22,34 +22,74 @@ using namespace std;
 
 
 void MergeSort(vector<int>& v, vector<int>::iterator left, vector<int>::iterator right) {
-    if (right == left || v.end() == left) {
+    if (right == std::next(left)) {
         return;
     }
     auto mid = left +  distance(left, right)/2;
+    for (auto it = left; it != mid; ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+    for (auto it = mid; it != right; ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
     MergeSort(v, left, mid);
-    MergeSort(v, mid + 1, right);
+    MergeSort(v, mid, right);
     vector<int> temp;
-    auto it1 = left, it2 = mid + 1;
-    while(it1 != std::next(mid) && it2 != v.end()) {
+    auto it1 = left;
+    auto it2 = mid;
+    while(it1 != mid && it2 != right) {
+        cout << "*it1 => " << *it1 << endl;
+        cout << "*it2 => " << *it1 << endl;
         if (*it1 < *it2) {
             temp.push_back(*it1++);
         } else {
             temp.push_back(*it2++);
         }
     }
-    while (it1 != std::next(mid)) {
+    while (it1 != mid) {
         temp.push_back(*it1++);
     }
-    while (it2 != v.end()) {
+    while (it2 != right) {
         temp.push_back(*it2++);
     }
 
     for (auto it = temp.begin(); it!= temp.end(); ++it) {
-        *left = *it;
-        ++left;
+        *left++ = *it;
     }
 
     return;
+}
+
+void merge_sort(vector<int>& v, int left, int right) {
+    if (left >= right) {
+        return;
+    }
+    int mid = left + (right - left)/2;
+    merge_sort(v, left, mid);
+    merge_sort(v, mid + 1, right);
+    int n = right - left + 1;
+    vector<int> temp(n, 0);
+    int index = 0;
+    int l = left;
+    int r = mid + 1;
+    for (; l <= mid && r <= right; ) {
+        if (v[l] <= v[r]) {
+            temp[index ++] = v[l++];
+        } else {
+            temp[index ++] = v[r++];
+        }
+    }
+    while (l <= mid) {
+        temp[index ++] = v[l++];
+    }
+    while (r <= right)  {
+        temp[index ++] = v[r++];
+    }
+    for (int i = 0; i < n; ++i) {
+        v[left ++ ] = temp[i];
+    }
 }
 
 void printv(vector<int>& v) {
@@ -62,8 +102,12 @@ void printv(vector<int>& v) {
 
 int main(int argc, const char *argv[])
 {
-    vector<int> v = {12, 11, 13, 5, 6, 7};
+    vector<int> v = {12, 11, 13, 5, 6, 7, 4, 3, 2, 1};
+    cout << "before sort =>";
+    printv(v);
     MergeSort(v, v.begin(), v.end());
+    //merge_sort(v, 0, 9);
+    cout << "after sort =>";
     printv(v);
     return 0;
 }
