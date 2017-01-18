2a329c46e820aba8a0a79c1d85ffc2bd1897a057
yinchunxiang <yinchunxiang@gmail.com>
2017-01-18 15:21:51 +0800

    add comb sort

--- 20007.CombSort/main.cc
+++ 20007.CombSort/main.cc
@@ -13,10 +13,56 @@
  * @brief 
  * 
  **/ 
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int next_gap(int gap) {
+    gap = gap * 10 / 13;
+    if (gap < 1) {
+        return 1;
+    }
+    return gap;
+}
+
+void CombSort(std::vector<int>& v) {
+    int n = v.size();
+    int gap = n;
+
+    bool swapped = true;
+
+    while (gap != 1 || swapped) {
+        gap = next_gap(gap);
+        swapped = false;
+        for (int i = 0; i < n - gap; ++i) {
+            if (v[i] > v[i + gap]) {
+                std::swap(v[i], v[i + gap]);
+                swapped = true;
+            }
+        }
+    }
+    return;
+
+}
+
+void printv(const std::vector<int> &v) {
+    for (const auto &i: v) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
  
 int main(int argc, const char *argv[])
 {
-    
+    std::vector<int> v = {8, 4, 1, 56, 3, -44, 23, -6, 28, 0};
+    cout <<  "before sort => ";
+    printv(v);
+    CombSort(v);
+    cout <<  "after sort => ";
+    printv(v);
+
     return 0;
 }
 
