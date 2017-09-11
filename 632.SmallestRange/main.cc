0b2eea4edfeb7739dc8e7071d0297ec2f5f40115
yinchunxiang <>
2017-09-11 09:53:44 +0800

    update 632.SmallestRange

--- 632.SmallestRange/main.cc
+++ 632.SmallestRange/main.cc
@@ -17,19 +17,35 @@
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        vector<int> all;
-        // 初始区间
-        int start = 0;
-        int end = 0;
-        int min_range = INT_MAX;
-        int num_list = nums.size();
-        // 用来记录有哪些list出现在区间中
-        vector<int> occurs(num_list, 0);
-        // 用来记录数字在哪些列表出现过
-        unordered_map<int, vector<int>> 
-        for (auto &row : nums) {
-
+        typedef vector<int>::iterator VecIntIter;
+        struct comp {
+            bool operator()(const pair<VecIntIter, VecIntIter> &a, const pair<VecIntIter, VecIntIter> &b) {
+                return (*a).first > (*b.first);
+            }
         }
+
+        int left = INT_MAX;
+        int right = INT_MIN;
+
+        priority_queue<VecIntIter, vector<VecIntIter>, comp> pq;
+        for (auto &row : nums) {
+            left = min(left, row[0]);
+            right = max(right, row[0]);
+            pq.push({row.begin(), row.end()});
+        }
+        vector<int> result = {left, right};
+        while (true) {
+            auto p = pq.top();
+            pq.pop();
+            ++p.first;
+            if (p.first == p.second) {
+                break;
+            }
+            pq.push(p);
+            left = *(pq.top().first);
+            right = max(right, *(p.first));
+        }
+        return result;
         
     }
 };
