1e43fdfd0ad756664f9f0834738f3fc53fb43564
yinchunxiang <yinchunxiang@lvwan.com>
2017-11-29 09:08:18 +0800

    update 726.NumberOfAtoms/

--- 726.NumberOfAtoms/3.cc
+++ 726.NumberOfAtoms/3.cc
@@ -2,6 +2,11 @@
 // Created by yinchunxiang on 28/11/2017.
 //
 
+#include <string>
+#include <map>
+
+using namespace std;
+
 class Solution {
 public:
     string countOfAtoms(string formula) {
@@ -15,7 +20,50 @@ public:
             }
         }
         return output;
-
     }
+
+private:
+    void merge(map<string, int>& counts, map<string, int>& cnt, int times) {
+        for (auto& p : cnt) {
+            counts[p.first] += p.second * times;
+        }
+    }
+
+    int parseDigits(const string& s, int& i) {
+        int start = i;
+        while (i < s.size() && isdigit(static_cast<unsigned char>(s[i]))) {
+            ++i;
+        }
+        int digits = (start == i) ? 1 : stoi(s.substr(start, i - start));
+        return digits;
+    }
+
+    map<string, int> parseFormula(const string& s, int& i) {
+        int n = (int) s.size();
+        map<string, int> counts;
+        while (i < n && s[i] != ')') {
+            map<string, int> cnt = parseUnit(s, i);
+            merge(counts, cnt, 1);
+        }
+        return counts;
+    }
+
+    map<string, int> parseUnit(const string& s, int& i) {
+        map<string, int> counts;
+        if ('(' == s[i]) {
+            map<string, int> cnt = parseFormula(s, ++i);
+            int digits = parseDigits(s, ++i);
+            merge(counts, cnt, digits);
+        } else {
+            int start = i++;
+            while (i < s.size() && islower(static_cast<unsigned char>(s[i]))) {
+                ++i;
+            }
+            string atom = s.substr(start, i - start);
+            int digits = parseDigits(s, i);
+            counts[atom] += digits;
+        }
+        return counts;
+    };
 };
 
---
+++ 726.NumberOfAtoms/4.cc
@@ -0,0 +1,14 @@
+//
+// Created by alexkkk on 11/28/17.
+//
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string countOfAtoms(string formula) {
+
+    }
+};
